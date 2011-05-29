9a93dd7cbf279e596425238cb7c6bbf9f84a4d6c
hgye <greenar@gmail.com>
2011-05-29 23:57:59 +0800

    0529 night, add some xml process

--- clicommand.py
+++ clicommand.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+C#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from xml.dom import minidom
@@ -13,6 +13,51 @@ class Command:
         self.dom = minidom.parse(fd)
         close()
 
-    def packMsg():
-        
+    def handleCommand:
+        """
+        handle xml segment
+        <Command ...>
+        """
+        comlist = self.dom.getElementsByTagName("Command")
+
+        for com in comlist:
+            getVersion(com)
+            getName(com)
+            getRetType(com)
+            #getRetVal(com)
+
+    def getVersion(dom):
+        return dom.getAttribute(u'version').encode('ascii', 'ignore')
+
+    def getName(dom):
+        return dom.getAttribute(u'command_name').encode('ascii', 'ignore')
     
+    def getRetType(dom):
+        return dom.getAttribute(u'return_type').encode('ascii', 'ignore')
+
+    def handleParameter:
+        """
+        handle xml segment
+        <Paramter ...>
+        """
+        parlist = self.dom.getElementsByTagName("Parameter")
+        for par in parlist:
+            getParInOut(par)
+            getParType(par)
+            getParVal(par)
+
+    def getParInOut(par):
+        return par.getAttribute(u'in_or_out').encode('ascii'. 'ignore')
+
+    def getParType(par):
+        return par.getAttribute(u'data_type').encode('ascii', 'ignore')
+
+    def getParVal(par):
+        return par.getAttribute(u'vale').encode('ascii', 'ignore')
+
+    def getvalue(key):
+        key.value
+
+
+    def packMsg():
+        comlist[0].attributes[u"version"]
