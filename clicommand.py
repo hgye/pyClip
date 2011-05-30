be12b5f7fd8fa22b176116e9aa5bb58de0efd37b
hgye <greenar@gmail.com>
2011-05-30 23:38:39 +0800

    0530, xml->msg

--- clicommand.py
+++ clicommand.py
@@ -1,7 +1,18 @@
-C#!/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 from xml.dom import minidom
+
+typedic = {'Byte':('B', 1),
+           'Word':('W', 2),
+           'Dword':('D', 4),
+           'String':('S', 0),
+           'Void':('V', 0) }
+
+inoutdic = {'out':'O',
+            'in':'I'}
 
 class Command:
     """
@@ -9,22 +20,32 @@ class Command:
     every instance is a cli command
     """
     def __init__(xmlfile):
-        fd = open(xmlfile)
+        self.xmlfile = xmlfile
+        fd = open(self.xmlfile)
         self.dom = minidom.parse(fd)
-        close()
+        close(fd)
 
-    def handleCommand:
+    def handleCommand():
         """
         handle xml segment
         <Command ...>
         """
         comlist = self.dom.getElementsByTagName("Command")
 
+        if 1 != len(comlist):
+            print "There are more than 1 <Command> Node in file %s"%(os.path.abspath(self.xmlfile) )
+            return # @todo, is that OK, try.. catch, error case
+
         for com in comlist:
-            getVersion(com)
-            getName(com)
-            getRetType(com)
+            ver = getVersion(com)
+            com_name = getName(com)
+            ret_type = getRetType(com)
             #getRetVal(com)
+
+        msg_head = ''.join(['REQ   ', ver])
+        msg_com  = ''.join( [chr(len(com_name)), com_name ])
+        msg_ret  = ''.join([typedic[ret_type][0], typedic[ret_type][1] ])
+
 
     def getVersion(dom):
         return dom.getAttribute(u'version').encode('ascii', 'ignore')
@@ -35,16 +56,31 @@ class Command:
     def getRetType(dom):
         return dom.getAttribute(u'return_type').encode('ascii', 'ignore')
 
-    def handleParameter:
+    def handleParameters():
         """
         handle xml segment
         <Paramter ...>
         """
         parlist = self.dom.getElementsByTagName("Parameter")
+
+        parnum = len(parlist)
+        if 12 >= parnum:
+            print "parameter number too much, parnum = %d"%parnum
+            return # @todo, error case
+        
         for par in parlist:
-            getParInOut(par)
-            getParType(par)
-            getParVal(par)
+            handleParameter(par)
+
+    def handleParameter(par):
+        inout = inoutdic(getParInOut(par))
+        par_type = getParType(par)
+        par_val = getParVal(par)
+
+        if inout == 'I':
+            msg_par = ''.join([inout, typedic[par_type][0], typedic[par_type][1], par_val ])
+        else inout == 'O':
+            msg_par = ''.join([inout, typedic[par_type][0], typedic[par_type][1] ])
+
 
     def getParInOut(par):
         return par.getAttribute(u'in_or_out').encode('ascii'. 'ignore')
@@ -55,9 +91,7 @@ class Command:
     def getParVal(par):
         return par.getAttribute(u'vale').encode('ascii', 'ignore')
 
-    def getvalue(key):
-        key.value
-
 
     def packMsg():
-        comlist[0].attributes[u"version"]
+        handleCommand()
+        handleParameters()
